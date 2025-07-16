27ea0cbfd77e6e340bca718e7fcb364db4ec7e5b
m-faisal930 <faisal.mr333@gmail.com>
2025-07-16 18:24:02 +0500

    cors solution

--- backend/server.js
+++ backend/server.js
@@ -11,12 +11,23 @@ const allowedOrigins = [
   'http://localhost:3000',
 ];
 
+
 app.use(
   cors({
-    origin: allowedOrigins,
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
     credentials: true,
   })
 );
+
+
+
+
 app.use(express.json());
 
 // Database connection
